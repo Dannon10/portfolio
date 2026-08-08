3de31cfd8a90167ef9faa6c9d885b492df82a3cf
Dannon Abayomi <abayomidannon10@gmail.com>
2026-08-08 17:21:20 +0100

    fixed deployemnt issue

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
-import './Services.css';
+import './services.css';
 
 gsap.registerPlugin(ScrollTrigger);
 
